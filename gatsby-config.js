4c0da40ff6aa754c92c8707b58f2f0ee37b21362
tom <tomphill@live.co.uk>
2019-05-02 21:30:18 +0100

    Add styled components for SSR

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
